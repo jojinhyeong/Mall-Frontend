21182a94d76837d87f331e5cd2e413c4b6cf15ad
JOJINHYEONG <whwlsgud111@naver.com>
2025-01-26 15:47:03 +0900

    frist commit

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BasicLayout from '../layout/BasicLayout';
+
+function AboutPage(props) {
+    return (
+        <BasicLayout>
+            <div className='{text-3xl}'>About Page</div>
+        </BasicLayout>
+    );
+}
+
+export default AboutPage;
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BasicLayout from '../layout/BasicLayout';
+
+function MainPage(props) {
+    return (
+        <BasicLayout>
+            <div className='{text-3xl}'>Main Page</div>
+        </BasicLayout>
+    );
+}
+
+export default MainPage;
